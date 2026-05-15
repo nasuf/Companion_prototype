e0e82bae742ef57ea5f39bb70ede2be5dc9384f2
nasuf <shanian713@gmail.com>
2026-05-15 12:04:50 +0800

    init commit

--- src/pages/music.js
+++ src/pages/music.js
@@ -2,19 +2,19 @@
 function music(){
   const wave=[66,112,52,126,78,108,64,96,48,132,84,116,70,101,56,124,88,136,58,104];
   const agentList=[
-    ["云烟成雨","房东的猫","云烟成雨 - Single","1 首","#1f6fff","#18c6c0",true,"https://is1-ssl.mzstatic.com/image/thumb/Music221/v4/23/32/23/2332234e-89aa-9aec-f589-48d518a304cc/3616840519694.jpg/600x600bb.jpg"],
-    ["夜空中最亮的星","逃跑计划","世界","10 首","#7c3cff","#1f6fff",false,"https://is1-ssl.mzstatic.com/image/thumb/Music115/v4/dc/85/ab/dc85ab94-26c9-5f50-3c89-d9a95b22ca1c/2910029.jpg/600x600bb.jpg"],
-    ["给你一瓶魔法药水","告五人","玫瑰凭证","8 首","#ff8a3d","#7c3cff",false,"https://is1-ssl.mzstatic.com/image/thumb/Music112/v4/86/22/53/86225309-0ac8-3ee6-3118-66ec50ddc8e7/196589268105.jpg/600x600bb.jpg"],
-    ["慢慢喜欢你","莫文蔚","慢慢喜欢你 - Single","1 首","#22c66b","#1f6fff",false,"https://is1-ssl.mzstatic.com/image/thumb/Music221/v4/c0/31/a7/c031a72d-f7b0-55bd-d5c6-a7223b8adaa4/cover.jpg/600x600bb.jpg"],
-    ["玫瑰少年","五月天","玫瑰少年 - Single","1 首","#18c6c0","#22c66b",false,"https://is1-ssl.mzstatic.com/image/thumb/Music113/v4/dc/86/ea/dc86ea9c-2ba0-e6f6-25f1-bee57d809089/1._-.jpg/600x600bb.jpg"],
-    ["如果可以","韦礼安","如果可以 - From THE FIRST TAKE","1 首","#1f6fff","#7c3cff",false,"https://is1-ssl.mzstatic.com/image/thumb/Music112/v4/d8/ca/64/d8ca640f-3b2d-55db-1f35-31f8f813d9d1/196589600639.jpg/600x600bb.jpg"]
+    ["云烟成雨","房东的猫","云烟成雨 - Single","1 首","#1f6fff","#18c6c0",true,"assets/prototype/remote/music-cover-01.jpg"],
+    ["夜空中最亮的星","逃跑计划","世界","10 首","#7c3cff","#1f6fff",false,"assets/prototype/remote/music-cover-02.jpg"],
+    ["给你一瓶魔法药水","告五人","玫瑰凭证","8 首","#ff8a3d","#7c3cff",false,"assets/prototype/remote/music-cover-03.jpg"],
+    ["慢慢喜欢你","莫文蔚","慢慢喜欢你 - Single","1 首","#22c66b","#1f6fff",false,"assets/prototype/remote/music-cover-04.jpg"],
+    ["玫瑰少年","五月天","玫瑰少年 - Single","1 首","#18c6c0","#22c66b",false,"assets/prototype/remote/music-cover-05.jpg"],
+    ["如果可以","韦礼安","如果可以 - From THE FIRST TAKE","1 首","#1f6fff","#7c3cff",false,"assets/prototype/remote/music-cover-06.jpg"]
   ];
   const userList=[
-    ["晴天","周杰伦","叶惠美","11 首","#7c3cff","#1f6fff",false,"https://is1-ssl.mzstatic.com/image/thumb/Music115/v4/45/8a/e4/458ae484-dc8b-5683-ce04-8d2948346462/JAY.jpg/600x600bb.jpg"],
-    ["我记得","赵雷","署前街少年","10 首","#18c6c0","#22c66b",false,"https://is1-ssl.mzstatic.com/image/thumb/Music126/v4/3e/fc/be/3efcbe87-236a-ad35-98c0-b075cd95a1b4/6976364784976.jpg/600x600bb.jpg"],
-    ["句号","邓紫棋","摩天动物园","13 首","#101820","#1f6fff",false,"https://is1-ssl.mzstatic.com/image/thumb/Music124/v4/a8/59/dd/a859ddfd-0f50-4987-0301-2117bd152c4c/886448203414.jpg/600x600bb.jpg"],
-    ["NEW BOY","朴树","我去2000年","11 首","#ff8a3d","#ffc936",false,"https://is1-ssl.mzstatic.com/image/thumb/Music124/v4/28/94/e0/2894e08b-05bc-d63c-23ee-4b7e834fb5f2/fengmian.jpg/600x600bb.jpg"],
-    ["小幸运","田馥甄","小幸运 - Single","1 首","#1f6fff","#18c6c0",false,"https://is1-ssl.mzstatic.com/image/thumb/Music115/v4/69/87/7f/69877fbc-cd2c-bb9b-2397-5f3ed14a04a8/Hebe_Little_Happiness_1400.jpg/600x600bb.jpg"]
+    ["晴天","周杰伦","叶惠美","11 首","#7c3cff","#1f6fff",false,"assets/prototype/remote/music-cover-07.jpg"],
+    ["我记得","赵雷","署前街少年","10 首","#18c6c0","#22c66b",false,"assets/prototype/remote/music-cover-08.jpg"],
+    ["句号","邓紫棋","摩天动物园","13 首","#101820","#1f6fff",false,"assets/prototype/remote/music-cover-09.jpg"],
+    ["NEW BOY","朴树","我去2000年","11 首","#ff8a3d","#ffc936",false,"assets/prototype/remote/music-cover-10.jpg"],
+    ["小幸运","田馥甄","小幸运 - Single","1 首","#1f6fff","#18c6c0",false,"assets/prototype/remote/music-cover-11.jpg"]
   ];
   const tabs=[["agent","小芜在播"],["user","我的收藏"]];
   const active=state.musicTab==="user"?"user":"agent";
